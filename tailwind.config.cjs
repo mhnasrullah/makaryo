a468512fe3421b344af8b1171589795fb6ad88f7
Hasan <hnasrul22@gmail.com>
2023-01-08 06:30:30 +0700

    set up tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors : {
+      white : "#F5F7FB",
+      blue : "#326DB7",
+      black : "#292D32",
+      grey : "#878484",
+      pureWhite : "#FFFFFF",
+      transparent : "#00000000"
+    }
   },
   plugins: [],
 }
