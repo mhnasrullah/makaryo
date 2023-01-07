a18f2c7b8bd2e5632ce9504e7be0a5d9e5bf8b62
Hasan <hnasrul22@gmail.com>
2023-01-08 06:17:23 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Lets build makaryo
+      </h1>
+    </div>
+  )
+}
+
+export default App
